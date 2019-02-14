f1a13e89a3b174ada21274982524a58f837d7710
Dean Lunz <deanlunz@createdbyx.com>
2019-02-14 12:58:37 -0800

    Added initial files

---
+++ CombineMeshes.cs
@@ -0,0 +1,76 @@
+// <copyright>
+//   Copyright (c) 2012 Codefarts
+//   All rights reserved.
+//   contact@codefarts.com
+//   http://www.codefarts.com
+// </copyright>
+
+namespace Codefarts.GeneralTools.Code
+{
+    using System;
+    using System.Collections.Generic;
+
+    using UnityEngine;
+
+    public class CombineMeshes
+    {
+        public static void Combine(MeshRenderer[] meshRenders, MeshFilter[] meshFilters, out Mesh mesh, out Material[] materials)
+        {
+            var meshInstanceList = new List<SkinMeshCombineUtility.MeshInstance>();
+            var materialList = new List<Material>();
+
+            // validate arguments
+            if (meshFilters == null)
+            {
+                throw new ArgumentNullException("meshFilters");
+            }
+
+            if (meshRenders== null)
+            {
+                throw new ArgumentNullException("meshRenders");
+            }
+            
+            if (meshRenders.Length != meshFilters.Length)
+            {
+                throw new ArgumentException("'meshRenders' & 'meshFilters' must be the same length.");
+            }
+
+            // build a list of MeshInstance types
+            for (var index = 0; index < meshRenders.Length; index++)
+            {
+                var meshRenderer = meshRenders[index];
+                
+                // Getting one by one
+                var meshFilter = meshFilters[index];
+
+                // Making changes to the Skinned Renderer
+                var instance = new SkinMeshCombineUtility.MeshInstance { mesh = meshFilter.sharedMesh };
+
+                // Getting All Materials
+                for (var i = 0; i < meshRenderer.sharedMaterials.Length; i++)
+                {
+                    materialList.Add(meshRenderer.sharedMaterials[i]);
+                }
+
+                if (meshRenderer != null && instance.mesh != null)
+                {
+                    // Getting subMesh
+                    for (var i = 0; i < meshFilter.sharedMesh.subMeshCount; i++)
+                    {
+                        instance.subMeshIndex = i;
+                        meshInstanceList.Add(instance);
+                    }
+                }
+            }
+
+            // combine meshes
+            var newMesh = SkinMeshCombineUtility.Combine(meshInstanceList.ToArray());
+            newMesh.RecalculateNormals();
+            newMesh.RecalculateBounds();
+            mesh = newMesh;
+
+            // Setting Materials
+            materials = materialList.ToArray();
+        }
+    }
+}
---
+++ Editor/MeshMerge.cs
@@ -0,0 +1,57 @@
+﻿namespace Codefarts.GeneralTools.Code.Utilities
+{
+    using UnityEditor;
+
+    using UnityEngine;
+
+    public class MeshMerge
+    {
+        /// <summary>
+        /// Provides unity menu item to open the window.
+        /// </summary>
+        [MenuItem("Window/Codefarts/General Utilities/Merge Selected Meshes")]
+        private static void MergeSelectedMeshes()
+        {
+            Material[] materials;
+            Mesh mesh;
+            var renderers = new MeshRenderer[2];
+            var meshFilters = new MeshFilter[2];
+
+            var index = 0;
+
+            foreach (var item in Selection.transforms)
+            {
+                var f = item.GetComponent<MeshFilter>();
+                var r = item.GetComponent<MeshRenderer>();
+                if (f != null && r != null)
+                {
+                    renderers[index] = r;
+                    meshFilters[index] = f;
+                    index++;
+                }
+
+                if (index == 2)
+                {
+                    break;
+                }
+            }
+
+            if (index == 2)
+            {
+                var newObject = new GameObject("Result");
+                var filter = newObject.AddComponent<MeshFilter>();
+                var renderer = newObject.AddComponent<MeshRenderer>();
+                CombineMeshes.Combine(renderers, meshFilters, out mesh, out materials);
+                filter.sharedMesh = mesh;
+                renderer.sharedMaterials = materials;
+            }
+            else
+            {
+                EditorUtility.DisplayDialog(
+                    "Information",
+                    "Must select 2 game objects that have a MeshRenderer & MeshFilter components on them!",
+                    "Accept");
+            }
+        }
+    }
+}
